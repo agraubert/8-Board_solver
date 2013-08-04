fbfe1c52c11b1c901d85b5db42d48062bc7bf017
captianjroot <captianjroot@live.com>
2013-08-03 22:15:30 -0500

    removed hardcoding from stoa function

--- 8board solver (better).py
+++ 8board solver (better).py
@@ -12,31 +12,40 @@ def atos(picture):
         final_string+="".join(row)
     return final_string
 
-def stoa(string):
-    final_grid=[[string[0], string[1], string[2]], [string[3], string[4], string[5]], [string[6], string[7], string[8]]]
-    return final_grid
+def stoa(string, leny):
+    lenx=(len(string))/leny
+    if(int(lenx)!=lenx):
+        return None
+    lenx=int(lenx)
+    final_array=[]
+    for y in range(leny):
+        current_row=[]
+        for x in range(lenx):
+            current_row.append(string[x+(y*lenx)])
+        final_array.append(current_row)
+    return final_array
 
 def generate_offspring(parent):
     children=[]
-    templateboard=[thing[:] for thing in stoa(parent)]
-    coord=locate_points(stoa(parent), " ")
+    templateboard=[thing[:] for thing in stoa(parent, 3)]
+    coord=locate_points(stoa(parent, 3), " ")
     if(coord[0]>=1): #move up
         templateboard[coord[0]][coord[1]]=templateboard[coord[0]-1][coord[1]]
         templateboard[coord[0]-1][coord[1]]=" "
         children.append(atos(templateboard))
-        templateboard=[thing[:] for thing in stoa(parent)]
+        templateboard=[thing[:] for thing in stoa(parent, 3)]
 
     if(coord[1]>=1): #move left
         templateboard[coord[0]][coord[1]]=templateboard[coord[0]][coord[1]-1]
         templateboard[coord[0]][coord[1]-1]=" "
         children.append(atos(templateboard))
-        templateboard=[thing[:] for thing in stoa(parent)]
+        templateboard=[thing[:] for thing in stoa(parent, 3)]
 
     if(coord[0]<=1): #move down
         templateboard[coord[0]][coord[1]]=templateboard[coord[0]+1][coord[1]]
         templateboard[coord[0]+1][coord[1]]=" "
         children.append(atos(templateboard))
-        templateboard=[thing[:] for thing in stoa(parent)]
+        templateboard=[thing[:] for thing in stoa(parent, 3)]
 
     if(coord[1]<=1): #move right
         templateboard[coord[0]][coord[1]]=templateboard[coord[0]][coord[1]+1]
@@ -50,10 +59,10 @@ def find_priority(current, goal):
     state_priority=0
     goal_locations=[]
     for point in range(1,9):
-        locations.append(locate_points(stoa(current), str(point)))
-        goal_locations.append(locate_points(stoa(goal), str(point)))
-    locations.append(locate_points(stoa(current), " "))
-    goal_locations.append(locate_points(stoa(goal), " "))
+        locations.append(locate_points(stoa(current, 3), str(point)))
+        goal_locations.append(locate_points(stoa(goal, 3), str(point)))
+    locations.append(locate_points(stoa(current, 3), " "))
+    goal_locations.append(locate_points(stoa(goal, 3), " "))
     for point in range(len(locations)):
         state_priority+=(abs(goal_locations[point][0]-locations[point][0])+abs(goal_locations[point][1]-locations[point][1]))
     return state_priority                  
@@ -75,7 +84,7 @@ if(len(raw3)==3):
 
 solution, explored=astar.solve(atos(board), atos(endset), generate_offspring, find_priority)
 for state in solution:
-    nstate=stoa(state)
+    nstate=stoa(state, 3)
     for a in range(len(nstate)):
         print("".join(nstate[a]))
     print("\n")
